5b1070953843912da4d19a3b4f4d1ceeec42b6eb
firenessmax <fireness@Fireness.local>
2014-12-08 17:58:41 -0300

    header problem

---
+++ cargarchivo.c
@@ -0,0 +1,28 @@
+#include "cargarchivo.h"
+#include<stdlib.h>
+
+float* leerArchivo (char *direccion){
+    
+    FILE *archivo;
+    char mensaje;
+    float lista[100000];
+    int n;
+    int i=0;
+    
+    archivo = fopen(direccion,"r");
+    
+    if(archivo == NULL){
+        printf("\nError al abrir el archivo");
+    } else {
+        n = fgetc(archivo);
+        char arr[n];
+        while (feof(archivo)==0){
+            mensaje = fgetc(archivo);
+            lista[i]= atoi(mensaje);
+            i++;
+        }
+    }
+    fclose(archivo);
+    
+    return lista;
+}
